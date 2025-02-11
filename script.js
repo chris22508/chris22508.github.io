b0fa3cf0baa848a1038370bf8f881365dba3ddc1
Christopher <157776193+chris22508@users.noreply.github.com>
2025-02-10 20:11:51 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
+document.getElementById("moveButton").addEventListener("mouseover", function() {
+    let newX = Math.random() * (window.innerWidth - this.offsetWidth);
+    let newY = Math.random() * (window.innerHeight - this.offsetHeight);
 
+    this.style.left = `${newX}px`;
+    this.style.top = `${newY}px`;
+});
